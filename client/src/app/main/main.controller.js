03195ce4e83932602522519398a6c97eba9a703b
Battulga Tsogtgerel <ts.battulga18@gmail.com>
2016-05-28 22:17:18 +0900

    Picture Upload

--- client/src/app/main/main.controller.js
+++ client/src/app/main/main.controller.js
@@ -13,4 +13,20 @@
     if (config.env === 'app') vm.route = config.app.host;
 
   }
+
+  function fileModel ($parse){
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs){
+        var model = $parse(attrs.fileModel);
+        var modelSetter =  model.assign;
+
+        element.bind('change', function(){
+          scope.$apply(function(){
+            modelSetter(scope, element[0].files[0]);
+          })
+        })
+      }
+    }
+  }
 })();
